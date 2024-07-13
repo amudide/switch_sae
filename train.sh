e27321681ec43b701cf6bc1a936206159f10b199
amudide <amudide@gmail.com>
2024-07-13 06:09:42 -0400

    flop matched runs

--- train.sh
+++ train.sh
@@ -10,5 +10,7 @@ python train-switch.py --gpu 5 --ks 32 --num_experts 32 --heavisides f t
 python train-switch.py --gpu 6 --ks 64 --num_experts 32 --heavisides f t
 python train-switch.py --gpu 7 --ks 128 --num_experts 32 --heavisides f t
 
+python train-switch-flop.py --gpu 3 --ks 8 32 64 128 --num_experts 8 --heavisides f
+
 cd /data/cb/scratch/amudide/switch_sae
 conda activate sae
