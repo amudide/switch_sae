dd345fd06b94765aef6b52b9327f8bbe1de672e8
amudide <amudide@gmail.com>
2024-07-13 23:13:33 -0400

    clean

--- can/run_pythia_sae_trainer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-python3 pythia_sae_trainer.py \
-    --save_dir /share/u/can/shift_eval/train_saes/trained_saes/pythia70m/sweep0709 \
-    --layer 4 \
-    # --no_wandb_logging \
-    # --dry_run \
