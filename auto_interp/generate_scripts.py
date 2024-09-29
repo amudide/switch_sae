52220f538e0243dd10a464e2a6c308ae774019c1
Joshua Engels <jengels@mit.edu>
2024-09-28 20:11:44 -0400

    Updated plot

--- auto_interp/generate_scripts.py
+++ auto_interp/generate_scripts.py
@@ -1,9 +1,18 @@
 # %%
 import glob
 
+import os
+
+# Change dir to two dirs up
+file_path = os.path.abspath(__file__)
+os.chdir(os.path.dirname(os.path.dirname(file_path)))
+
 # Get all paths in dictionaries
 dictionaries = glob.glob("dictionaries/**/*.pt", recursive=True)
 parent_dirs = set(["/".join(d.split("/")[:-1]) for d in dictionaries])
+
+# Change dir back
+os.chdir(os.path.dirname(file_path))
 
 # Write out run_all_feature_eval_generate.sh
 with open("run_all_feature_eval_generate.sh", "w") as f:
@@ -19,12 +28,12 @@ with open("run_all_feature_eval_eval.sh", "w") as f:
 # Only k = 64, and (fixed-width or topk)
 with open("run_all_feature_eval_generate_small.sh", "w") as f:
     for parent_dir in parent_dirs:
-        if "k64" in parent_dir and ("fixed-width" in parent_dir or "topk" in parent_dir):
+        if "k64" in parent_dir:
             f.write(f"python sae_feature_evals.py --sae_path {parent_dir} --to_do generate\n")
 
 # Write out run_all_feature_eval_eval_small.sh
 # Only k = 64, and (fixed-width or topk)
 with open("run_all_feature_eval_eval_small.sh", "w") as f:
     for parent_dir in parent_dirs:
-        if "k64" in parent_dir and ("fixed-width" in parent_dir or "topk" in parent_dir):
+        if "k64" in parent_dir:
             f.write(f"python sae_feature_evals.py --sae_path {parent_dir} --to_do eval\n")
